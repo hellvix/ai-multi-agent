4eef9d7b84f8ee419ce40d235e50df1a16286e91
E. Camilo <elvis.camilo@mosek.com>
2021-03-24 14:14:14 +0100

    Push

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ helpFunction()
 {
    echo "AI MAS Client - 2021 DTU Final Project"
    echo ""
-   echo "Usage: $0 -m level"
+   echo "Usage: $0 -m level -t time out -s speed"
    echo "\t-m level found inside ROOT/levels/"
-   echo "\t-t timeout"
-   echo "\t-s replay speed"
+   echo "\t-t server timeout"
+   echo "\t-s action replay speed"
    echo "\t-h display this message"
    echo ""
    exit 1
@@ -19,7 +19,6 @@ do
       m ) level="$OPTARG" ;;
       t ) timeout="$OPTARG" ;;
       s ) speed="$OPTARG" ;;
-      h ) helpFunction ;;
       ? ) helpFunction ;;
    esac
 done
@@ -27,21 +26,21 @@ done
 if [ -z "$level" ]
 then
    level="MAPF00.lvl"
-   echo "- No level given. Using default:" $level;
+   echo "- No level specified. Using default:" $level;
    
 fi
 
 if [ -z "$timeout" ]
 then
    timeout=18000
-   echo "- No timeout given. Using default:" $timeout;
+   echo "- No timeout gispecifiedven. Using default:" $timeout;
    
 fi
 
 if [ -z "$speed" ]
 then
    speed=150
-   echo "- No speed given. Using default:" $speed;
+   echo "- No speed specified. Using default:" $speed;
    
 fi
 
